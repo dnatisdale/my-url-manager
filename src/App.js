5255335ab732eddc8033df950fb0c9444c042a4a
Dan Tisdale <dant.grnt@gmail.com>
2025-07-29 21:52:59 -0700

    Updates
    Updates

--- src/App.js
+++ src/App.js
@@ -793,20 +793,15 @@ function App() {
           )}
           
           <div className="flex flex-col sm:flex-row gap-2 mb-4">
-            <div className="flex-1 relative">
-              <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 pointer-events-none">
-                https://
-              </span>
-              <input
-                type="text"
-                value={inputUrl.replace('https://', '')}
-                onChange={e => setInputUrl('https://' + e.target.value.replace(/^https?:\/\//, ''))}
-                onKeyPress={handleKeyPress}
-                placeholder="Enter URL (without https://)"
-                className="w-full pl-16 pr-3 py-2 md:px-4 md:py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                disabled={!user}
-              />
-            </div>
+            <input
+              type="text"
+              value={inputUrl}
+              onChange={e => setInputUrl(e.target.value)}
+              onKeyPress={handleKeyPress}
+              placeholder="Enter URL..."
+              className="flex-1 px-3 py-2 md:px-4 md:py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              disabled={!user}
+            />
             <div className="flex gap-2">
               <select
                 value={selectedCategory}
